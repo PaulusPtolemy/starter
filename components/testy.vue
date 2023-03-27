65321c6e00b1200206a1d3125aa56914431aeffe
Paul Bartov <domoed132@gmail.com>
2023-03-27 15:13:15 +0300

    init project

---
+++ components/testy.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>
+    TESTY
+  </h1>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'testy',
+})
+</script>
+
+<script setup lang="ts">
+</script>
